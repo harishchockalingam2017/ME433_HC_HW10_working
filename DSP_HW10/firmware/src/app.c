45cd259e49a91a88de0105e86c8a3bc342fc420c
harishchockalingam <harishchockalingam2017@u.northwestern.edu>
2017-05-28 20:51:48 -0500

    fixed code

--- DSP_HW10/firmware/src/app.c
+++ DSP_HW10/firmware/src/app.c
@@ -6,10 +6,8 @@
   
   File Name:
     app.c
-
   Summary:
     This file contains the source code for the MPLAB Harmony application.
-
   Description:
     This file contains the source code for the MPLAB Harmony application.  It 
     implements the logic of the application's state machine and it may call 
@@ -24,15 +22,12 @@
 // DOM-IGNORE-BEGIN
 /*******************************************************************************
 Copyright (c) 2013-2014 released Microchip Technology Inc.  All rights reserved.
-
 Microchip licenses to you the right to use, modify, copy and distribute
 Software only when embedded on a Microchip microcontroller or digital signal
 controller that is integrated into your product or third party product
 (pursuant to the sublicense terms in the accompanying license agreement).
-
 You should refer to the license agreement accompanying this Software for
 additional information regarding your rights and obligations.
-
 SOFTWARE AND DOCUMENTATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
 EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF
 MERCHANTABILITY, TITLE, NON-INFRINGEMENT AND FITNESS FOR A PARTICULAR PURPOSE.
@@ -54,7 +49,9 @@ SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
 // *****************************************************************************
 
 #include "app.h"
-
+#include <xc.h>
+#include <stdio.h>
+#include "i2c_lib.h"
 // *****************************************************************************
 // *****************************************************************************
 // Section: Global Data Definitions
@@ -63,18 +60,34 @@ SUBSTITUTE GOODS, TECHNOLOGY, SERVICES, OR ANY CLAIMS BY THIRD PARTIES
 
 // *****************************************************************************
 /* Application Data
-
   Summary:
     Holds application data
-
   Description:
     This structure holds the application's data.
-
   Remarks:
     This structure should be initialized by the APP_Initialize function.
     
     Application strings and buffers are be defined outside this structure.
 */
+uint8_t APP_MAKE_BUFFER_DMA_READY dataOut[APP_READ_BUFFER_SIZE];
+uint8_t APP_MAKE_BUFFER_DMA_READY readBuffer[APP_READ_BUFFER_SIZE];
+#define NELEMS(x) (sizeof(x)/sizeof(x[0]))
+int len; int i = 0; int m=0; int mi=0;
+int imuT;
+
+short Data[7];
+char rCMD=0;
+int sTime = 0;
+
+short MAF_data[4]={0};
+short arr_Size=NELEMS(MAF_data);
+long int sum=0;
+int average;
+float IIRp=0; float averageIIR; float a=0.3; float b=0.7;
+
+int fi=0,fi2=0, FIR_data[4]={0},fir_arrSize=NELEMS(FIR_data);
+float FIRsum,FIRaverage;
+float SPweight[4]={0.0350, 0.4650, 0.4650, 0.0350};
 
 APP_DATA appData;
 
@@ -107,70 +120,438 @@ APP_DATA appData;
 /*******************************************************************************
   Function:
     void APP_Initialize ( void )
-
   Remarks:
     See prototype in app.h.
  */
 
-void APP_Initialize ( void )
-{
+USB_DEVICE_CDC_EVENT_RESPONSE APP_USBDeviceCDCEventHandler
+(
+        USB_DEVICE_CDC_INDEX index,
+        USB_DEVICE_CDC_EVENT event,
+        void * pData,
+        uintptr_t userData
+        ) {
+    APP_DATA * appDataObject;
+    appDataObject = (APP_DATA *) userData;
+    USB_CDC_CONTROL_LINE_STATE * controlLineStateData;
+
+    switch (event) {
+        case USB_DEVICE_CDC_EVENT_GET_LINE_CODING:
+
+            /* This means the host wants to know the current line
+             * coding. This is a control transfer request. Use the
+             * USB_DEVICE_ControlSend() function to send the data to
+             * host.  */
+
+            USB_DEVICE_ControlSend(appDataObject->deviceHandle,
+                    &appDataObject->getLineCodingData, sizeof (USB_CDC_LINE_CODING));
+
+            break;
+
+        case USB_DEVICE_CDC_EVENT_SET_LINE_CODING:
+
+            /* This means the host wants to set the line coding.
+             * This is a control transfer request. Use the
+             * USB_DEVICE_ControlReceive() function to receive the
+             * data from the host */
+
+            USB_DEVICE_ControlReceive(appDataObject->deviceHandle,
+                    &appDataObject->setLineCodingData, sizeof (USB_CDC_LINE_CODING));
+
+            break;
+
+        case USB_DEVICE_CDC_EVENT_SET_CONTROL_LINE_STATE:
+
+            /* This means the host is setting the control line state.
+             * Read the control line state. We will accept this request
+             * for now. */
+
+            controlLineStateData = (USB_CDC_CONTROL_LINE_STATE *) pData;
+            appDataObject->controlLineStateData.dtr = controlLineStateData->dtr;
+            appDataObject->controlLineStateData.carrier = controlLineStateData->carrier;
+
+            USB_DEVICE_ControlStatus(appDataObject->deviceHandle, USB_DEVICE_CONTROL_STATUS_OK);
+
+            break;
+
+        case USB_DEVICE_CDC_EVENT_SEND_BREAK:
+
+            /* This means that the host is requesting that a break of the
+             * specified duration be sent. Read the break duration */
+
+            appDataObject->breakData = ((USB_DEVICE_CDC_EVENT_DATA_SEND_BREAK *) pData)->breakDuration;
+
+            /* Complete the control transfer by sending a ZLP  */
+            USB_DEVICE_ControlStatus(appDataObject->deviceHandle, USB_DEVICE_CONTROL_STATUS_OK);
+
+            break;
+
+        case USB_DEVICE_CDC_EVENT_READ_COMPLETE:
+
+            /* This means that the host has sent some data*/
+            appDataObject->isReadComplete = true;
+            break;
+
+        case USB_DEVICE_CDC_EVENT_CONTROL_TRANSFER_DATA_RECEIVED:
+
+            /* The data stage of the last control transfer is
+             * complete. For now we accept all the data */
+
+            USB_DEVICE_ControlStatus(appDataObject->deviceHandle, USB_DEVICE_CONTROL_STATUS_OK);
+            break;
+
+        case USB_DEVICE_CDC_EVENT_CONTROL_TRANSFER_DATA_SENT:
+
+            /* This means the GET LINE CODING function data is valid. We dont
+             * do much with this data in this demo. */
+            break;
+
+        case USB_DEVICE_CDC_EVENT_WRITE_COMPLETE:
+
+            /* This means that the data write got completed. We can schedule
+             * the next read. */
+
+            appDataObject->isWriteComplete = true;
+            break;
+
+        default:
+            break;
+    }
+
+    return USB_DEVICE_CDC_EVENT_RESPONSE_NONE;
+}
+
+/***********************************************
+ * Application USB Device Layer Event Handler.
+ ***********************************************/
+void APP_USBDeviceEventHandler(USB_DEVICE_EVENT event, void * eventData, uintptr_t context) {
+    USB_DEVICE_EVENT_DATA_CONFIGURED *configuredEventData;
+
+    switch (event) {
+        case USB_DEVICE_EVENT_SOF:
+
+            /* This event is used for switch debounce. This flag is reset
+             * by the switch process routine. */
+            appData.sofEventHasOccurred = true;
+            break;
+
+        case USB_DEVICE_EVENT_RESET:
+
+            /* Update LED to show reset state */
+
+            appData.isConfigured = false;
+
+            break;
+
+        case USB_DEVICE_EVENT_CONFIGURED:
+
+            /* Check the configuratio. We only support configuration 1 */
+            configuredEventData = (USB_DEVICE_EVENT_DATA_CONFIGURED*) eventData;
+            if (configuredEventData->configurationValue == 1) {
+                /* Update LED to show configured state */
+
+                /* Register the CDC Device application event handler here.
+                 * Note how the appData object pointer is passed as the
+                 * user data */
+
+                USB_DEVICE_CDC_EventHandlerSet(USB_DEVICE_CDC_INDEX_0, APP_USBDeviceCDCEventHandler, (uintptr_t) & appData);
+
+                /* Mark that the device is now configured */
+                appData.isConfigured = true;
+
+            }
+            break;
+
+        case USB_DEVICE_EVENT_POWER_DETECTED:
+
+            /* VBUS was detected. We can attach the device */
+            USB_DEVICE_Attach(appData.deviceHandle);
+            break;
+
+        case USB_DEVICE_EVENT_POWER_REMOVED:
+
+            /* VBUS is not available any more. Detach the device. */
+            USB_DEVICE_Detach(appData.deviceHandle);
+            break;
+
+        case USB_DEVICE_EVENT_SUSPENDED:
+
+            /* Switch LED to show suspended state */
+            break;
+
+        case USB_DEVICE_EVENT_RESUMED:
+        case USB_DEVICE_EVENT_ERROR:
+        default:
+            break;
+    }
+}
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Local Functions
+// *****************************************************************************
+// *****************************************************************************
+
+/*****************************************************
+ * This function is called in every step of the
+ * application state machine.
+ *****************************************************/
+
+bool APP_StateReset(void) {
+    /* This function returns true if the device
+     * was reset  */
+
+    bool retVal;
+
+    if (appData.isConfigured == false) {
+        appData.state = APP_STATE_WAIT_FOR_CONFIGURATION;
+        appData.readTransferHandle = USB_DEVICE_CDC_TRANSFER_HANDLE_INVALID;
+        appData.writeTransferHandle = USB_DEVICE_CDC_TRANSFER_HANDLE_INVALID;
+        appData.isReadComplete = true;
+        appData.isWriteComplete = true;
+        retVal = true;
+    } else {
+        retVal = false;
+    }
+
+    return (retVal);
+}
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Initialization and State Machine Functions
+// *****************************************************************************
+// *****************************************************************************
+
+/*******************************************************************************
+  Function:
+    void APP_Initialize ( void )
+  Remarks:
+    See prototype in app.h.
+ */
+
+void APP_Initialize(void) {
     /* Place the App state machine in its initial state. */
     appData.state = APP_STATE_INIT;
 
-    
-    /* TODO: Initialize your application's state machine and other
-     * parameters.
-     */
-}
+    /* Device Layer Handle  */
+    appData.deviceHandle = USB_DEVICE_HANDLE_INVALID;
 
+    /* Device configured status */
+    appData.isConfigured = false;
+
+    /* Initial get line coding state */
+    appData.getLineCodingData.dwDTERate = 9600;
+    appData.getLineCodingData.bParityType = 0;
+    appData.getLineCodingData.bParityType = 0;
+    appData.getLineCodingData.bDataBits = 8;
+
+    /* Read Transfer Handle */
+    appData.readTransferHandle = USB_DEVICE_CDC_TRANSFER_HANDLE_INVALID;
+
+    /* Write Transfer Handle */
+    appData.writeTransferHandle = USB_DEVICE_CDC_TRANSFER_HANDLE_INVALID;
+
+    /* Intialize the read complete flag */
+    appData.isReadComplete = true;
+
+    /*Initialize the write complete flag*/
+    appData.isWriteComplete = true;
+
+    /* Reset other flags */
+    appData.sofEventHasOccurred = false;
+    //appData.isSwitchPressed = false;
+
+    /* Set up the read buffer */
+    appData.readBuffer = &readBuffer[0];
+    
+    I2C_master_setup();
+
+    sTime = _CP0_GET_COUNT();
+}
 
 /******************************************************************************
   Function:
     void APP_Tasks ( void )
-
   Remarks:
     See prototype in app.h.
  */
 
-void APP_Tasks ( void )
-{
+void APP_Tasks(void) {
+    /* Update the application state machine based
+     * on the current state */
 
-    /* Check the application's current state. */
-    switch ( appData.state )
-    {
-        /* Application's initial state. */
+    switch (appData.state) {
         case APP_STATE_INIT:
-        {
-            bool appInitialized = true;
-       
-        
-            if (appInitialized)
-            {
-            
-                appData.state = APP_STATE_SERVICE_TASKS;
+
+            /* Open the device layer */
+            appData.deviceHandle = USB_DEVICE_Open(USB_DEVICE_INDEX_0, DRV_IO_INTENT_READWRITE);
+
+            if (appData.deviceHandle != USB_DEVICE_HANDLE_INVALID) {
+                /* Register a callback with device layer to get event notification (for end point 0) */
+                USB_DEVICE_EventHandlerSet(appData.deviceHandle, APP_USBDeviceEventHandler, 0);
+
+                appData.state = APP_STATE_WAIT_FOR_CONFIGURATION;
+            } else {
+                /* The Device Layer is not ready to be opened. We should try
+                 * again later. */
+            }
+
+            break;
+
+        case APP_STATE_WAIT_FOR_CONFIGURATION:
+
+            /* Check if the device was configured */
+            if (appData.isConfigured) {
+                /* If the device is configured then lets start reading */
+                appData.state = APP_STATE_SCHEDULE_READ;
             }
             break;
-        }
 
-        case APP_STATE_SERVICE_TASKS:
-        {
-        
+        case APP_STATE_SCHEDULE_READ:
+
+            if (APP_StateReset()) {
+                break;
+            }
+
+            /* If a read is complete, then schedule a read
+             * else wait for the current read to complete */
+
+            appData.state = APP_STATE_WAIT_FOR_READ_COMPLETE;
+            if (appData.isReadComplete == true) {
+                appData.isReadComplete = false;
+                appData.readTransferHandle = USB_DEVICE_CDC_TRANSFER_HANDLE_INVALID;
+
+                USB_DEVICE_CDC_Read(USB_DEVICE_CDC_INDEX_0,
+                        &appData.readTransferHandle, appData.readBuffer,
+                        APP_READ_BUFFER_SIZE);
+                if(readBuffer[0]=='r'){
+                    rCMD=1;
+                }
+                if (appData.readTransferHandle == USB_DEVICE_CDC_TRANSFER_HANDLE_INVALID) {
+                    appData.state = APP_STATE_ERROR;
+                    break;
+                }
+            }
+
             break;
-        }
 
-        /* TODO: implement your application state machine.*/
-        
+        case APP_STATE_WAIT_FOR_READ_COMPLETE:
+        case APP_STATE_CHECK_TIMER:
 
-        /* The default state should never be executed. */
+            if (APP_StateReset()) {
+                break;
+            }
+
+            /* Check if a character was received or a switch was pressed.
+             * The isReadComplete flag gets updated in the CDC event handler. */
+
+            if (appData.isReadComplete || _CP0_GET_COUNT() - sTime> 240000) {
+                appData.state = APP_STATE_SCHEDULE_WRITE;
+            }
+
+            break;
+
+
+        case APP_STATE_SCHEDULE_WRITE:
+
+            if (APP_StateReset()) {
+                break;
+            }
+
+            /* Setup the write */
+
+            appData.writeTransferHandle = USB_DEVICE_CDC_TRANSFER_HANDLE_INVALID;
+            appData.isWriteComplete = false;
+            appData.state = APP_STATE_WAIT_FOR_WRITE_COMPLETE;
+
+            if (appData.isReadComplete) {
+                USB_DEVICE_CDC_Write(USB_DEVICE_CDC_INDEX_0,
+                        &appData.writeTransferHandle,
+                        appData.readBuffer, 1,
+                        USB_DEVICE_CDC_TRANSFER_FLAGS_DATA_COMPLETE);
+
+            } else {
+                
+                if(rCMD){
+                    
+                    for(i=0;i<100;i=i+1){
+                        imuT=_CP0_GET_COUNT();
+                        while(_CP0_GET_COUNT() - imuT > (240000)){;}
+                        
+                        IMU_read_multiple(IMU_ADD, 0x20, Data, 7);
+                        
+                        //MAF
+                        MAF_data[m]=Data[6];
+                        m++;
+                        if(m==arr_Size){
+                            m=0;
+                        }
+                        for(mi=0;mi<arr_Size;mi=mi+1){
+                            sum=sum + MAF_data[mi]; 
+                        }
+                        average=sum/arr_Size;
+                        sum=0;
+                        
+                        //IIR
+                        averageIIR=IIRp*a+Data[6]*b;
+                        IIRp=Data[6];
+                        
+                        //FIR
+                        FIR_data[fi]=Data[6];
+                        fi=fi+1;
+                        if(fi==fir_arrSize){
+                            fi=0;
+                        }
+                        for(fi2=0;fi2<fir_arrSize;fi2=fi2+1){
+                            FIRsum=FIRsum+SPweight[(fi+fir_arrSize-fi2)%fir_arrSize]*FIR_data[fi2];
+                        }
+                        FIRaverage=FIRsum;
+                        FIRsum=0;
+                            
+                        
+                        len = sprintf(dataOut, "%d raw az:%d MAF:%d, FIR:%9.2f IIR:%9.2f\r\n", 
+                                i+1,Data[6], average,FIRaverage, averageIIR);
+                        
+                        USB_DEVICE_CDC_Write(USB_DEVICE_CDC_INDEX_0,
+                        &appData.writeTransferHandle, dataOut, len,
+                        USB_DEVICE_CDC_TRANSFER_FLAGS_DATA_COMPLETE);
+                    }
+                    rCMD=0;
+                }else{
+                    len=1; dataOut[0]=0;
+                }
+                
+                USB_DEVICE_CDC_Write(USB_DEVICE_CDC_INDEX_0,
+                        &appData.writeTransferHandle, dataOut, len,
+                        USB_DEVICE_CDC_TRANSFER_FLAGS_DATA_COMPLETE);
+                sTime = _CP0_GET_COUNT();
+            }
+            break;
+
+        case APP_STATE_WAIT_FOR_WRITE_COMPLETE:
+
+            if (APP_StateReset()) {
+                break;
+            }
+
+            /* Check if a character was sent. The isWriteComplete
+             * flag gets updated in the CDC event handler */
+
+            if (appData.isWriteComplete == true) {
+                appData.state = APP_STATE_SCHEDULE_READ;
+            }
+
+            break;
+
+        case APP_STATE_ERROR:
+            break;
         default:
-        {
-            /* TODO: Handle error in application's state machine. */
             break;
-        }
     }
 }
 
- 
+
 
 /*******************************************************************************
  End of File
